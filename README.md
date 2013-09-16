618bee508049b66a4b7187c3953f85636df4ed8e
Victor Itkin <victor.itkin@gmail.com>
2013-09-16 11:06:52 +0300

    Set version to '27.0.1'.
    Replace the Partner WSDL with email extension with the one for the API 9.0 (Use in Email-To-Case Agent).
    Remove the Partner WSDL with email extension.
    Add and/or correct licenses headers and files.
    Multiple corrections in POM files.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Force.com Web Service Connector (WSC)
+
+The Force.com Web Service Connector (WSC) is a high performing web service client stack implemented using a streaming parser. WSC also makes it much easier to use the Force.com API (Web Services/SOAP or Asynchronous/BULK API). 
+
+## Building WSC
+    git clone git@github.com:vitkin/sfdc-wsc.git
+    mvn clean package
+
+## Generating Stubs From WSDLs
+    Use or replace the WSDL files located under the sub-modules folders `sfdc-wsc-ws-*` and build those sub-modules or the rebuild the entire project.
+
+## Write Application Code
+The following sample illustrates creating a connection and creating a new Account SObject.  Login is automatically handled by the Connector.
+
+```java
+    import com.sforce.soap.partner.*;
+    import com.sforce.soap.partner.sobject.*;
+    import com.sforce.ws.*;
+
+    public static void main(String args) {
+        ConnectorConfig config = new ConnectorConfig();
+        config.setUsername("username");
+        config.setPassword("password");
+
+        PartnerConnection connection = Connector.newConnection(config);
+        SObject account = new SObject();
+        account.setType("Account");
+        account.setField("Name", "My Account");
+        connection.create(new SObject[]{account});
+    }
--- sfdc-wsc-ws/src/main/java/com/sforce/ws/parser/MXParser.java
+++ sfdc-wsc-ws/src/main/java/com/sforce/ws/parser/MXParser.java
@@ -1,3 +1,33 @@
+/*
+ * #%L
+ * sfdc-wsc-ws
+ * %%
+ * Copyright (C) 2013 salesforce.com, inc.
+ * %%
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ *    Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *    following disclaimer.
+ * 
+ *    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ *    the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 
+ *    Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or
+ *    promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
 /* -*-             c-basic-offset: 4; indent-tabs-mode: nil; -*-  //------100-columns-wide------>|*/
 /*
  * Copyright (c) 2003 Extreme! Lab, Indiana University. All rights reserved.
@@ -3197,60 +3227,60 @@ public int nextToken()
 }
 
 /*
-* Indiana University Extreme! Lab Software License, Version 1.2
-*
-* Copyright (C) 2003 The Trustees of Indiana University.
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are
-* met:
-*
-* 1) All redistributions of source code must retain the above
-*    copyright notice, the list of authors in the original source
-*    code, this list of conditions and the disclaimer listed in this
-*    license;
-*
-* 2) All redistributions in binary form must reproduce the above
-*    copyright notice, this list of conditions and the disclaimer
-*    listed in this license in the documentation and/or other
-*    materials provided with the distribution;
-*
-* 3) Any documentation included with all redistributions must include
-*    the following acknowledgement:
-*
-*      "This product includes software developed by the Indiana
-*      University Extreme! Lab.  For further information please visit
-*      http://www.extreme.indiana.edu/"
-*
-*    Alternatively, this acknowledgment may appear in the software
-*    itself, and wherever such third-party acknowledgments normally
-*    appear.
-*
-* 4) The name "Indiana University" or "Indiana University
-*    Extreme! Lab" shall not be used to endorse or promote
-*    products derived from this software without prior written
-*    permission from Indiana University.  For written permission,
-*    please contact http://www.extreme.indiana.edu/.
-*
-* 5) Products derived from this software may not use "Indiana
-*    University" name nor may "Indiana University" appear in their name,
-*    without prior written permission of the Indiana University.
-*
-* Indiana University provides no reassurances that the source code
-* provided does not infringe the patent or any other intellectual
-* property rights of any other entity.  Indiana University disclaims any
-* liability to any recipient for claims brought by any other entity
-* based on infringement of intellectual property rights or otherwise.
-*
-* LICENSEE UNDERSTANDS THAT SOFTWARE IS PROVIDED "AS IS" FOR WHICH
-* NO WARRANTIES AS TO CAPABILITIES OR ACCURACY ARE MADE. INDIANA
-* UNIVERSITY GIVES NO WARRANTIES AND MAKES NO REPRESENTATION THAT
-* SOFTWARE IS FREE OF INFRINGEMENT OF THIRD PARTY PATENT, COPYRIGHT, OR
-* OTHER PROPRIETARY RIGHTS.  INDIANA UNIVERSITY MAKES NO WARRANTIES THAT
-* SOFTWARE IS FREE FROM "BUGS", "VIRUSES", "TROJAN HORSES", "TRAP
-* DOORS", "WORMS", OR OTHER HARMFUL CODE.  LICENSEE ASSUMES THE ENTIRE
-* RISK AS TO THE PERFORMANCE OF SOFTWARE AND/OR ASSOCIATED MATERIALS,
-* AND TO THE PERFORMANCE AND VALIDITY OF INFORMATION GENERATED USING
-* SOFTWARE.
-*/
+ * Indiana University Extreme! Lab Software License, Version 1.2
+ *
+ * Copyright (C) 2003 The Trustees of Indiana University.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1) All redistributions of source code must retain the above
+ *    copyright notice, the list of authors in the original source
+ *    code, this list of conditions and the disclaimer listed in this
+ *    license;
+ *
+ * 2) All redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the disclaimer
+ *    listed in this license in the documentation and/or other
+ *    materials provided with the distribution;
+ *
+ * 3) Any documentation included with all redistributions must include
+ *    the following acknowledgement:
+ *
+ *      "This product includes software developed by the Indiana
+ *      University Extreme! Lab.  For further information please visit
+ *      http://www.extreme.indiana.edu/"
+ *
+ *    Alternatively, this acknowledgment may appear in the software
+ *    itself, and wherever such third-party acknowledgments normally
+ *    appear.
+ *
+ * 4) The name "Indiana University" or "Indiana University
+ *    Extreme! Lab" shall not be used to endorse or promote
+ *    products derived from this software without prior written
+ *    permission from Indiana University.  For written permission,
+ *    please contact http://www.extreme.indiana.edu/.
+ *
+ * 5) Products derived from this software may not use "Indiana
+ *    University" name nor may "Indiana University" appear in their name,
+ *    without prior written permission of the Indiana University.
+ *
+ * Indiana University provides no reassurances that the source code
+ * provided does not infringe the patent or any other intellectual
+ * property rights of any other entity.  Indiana University disclaims any
+ * liability to any recipient for claims brought by any other entity
+ * based on infringement of intellectual property rights or otherwise.
+ *
+ * LICENSEE UNDERSTANDS THAT SOFTWARE IS PROVIDED "AS IS" FOR WHICH
+ * NO WARRANTIES AS TO CAPABILITIES OR ACCURACY ARE MADE. INDIANA
+ * UNIVERSITY GIVES NO WARRANTIES AND MAKES NO REPRESENTATION THAT
+ * SOFTWARE IS FREE OF INFRINGEMENT OF THIRD PARTY PATENT, COPYRIGHT, OR
+ * OTHER PROPRIETARY RIGHTS.  INDIANA UNIVERSITY MAKES NO WARRANTIES THAT
+ * SOFTWARE IS FREE FROM "BUGS", "VIRUSES", "TROJAN HORSES", "TRAP
+ * DOORS", "WORMS", OR OTHER HARMFUL CODE.  LICENSEE ASSUMES THE ENTIRE
+ * RISK AS TO THE PERFORMANCE OF SOFTWARE AND/OR ASSOCIATED MATERIALS,
+ * AND TO THE PERFORMANCE AND VALIDITY OF INFORMATION GENERATED USING
+ * SOFTWARE.
+ */
